686185d37db3ee9ded2e6cf1e3d1103d55363e45
andchiind <andchiind@github.com>
2020-07-20 14:51:30 +0200

    Trying to make testing work with Jest

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,1 +1,6 @@
 import { appendConnectionHTMLList, addLocalTracksToConnection, addVideoStream, addScreenCapture, advertiseFile, dataChannelReceive } from '../public/main.js';
+
+//import * as main from '../public/main.js';
+test('name is square', () => {
+    expect(main.ourID).toBe(0)
+})
