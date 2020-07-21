f6aed6442adeebc8d2f34d7fe0fe94adf4c808ab
Magne Johannes Erlandsen <magneje@github.com>
2020-07-21 16:08:14 +0200

    Add some tests in for 3D.js

--- tests/modules/3D.test.js
+++ tests/modules/3D.test.js
@@ -1,12 +1,21 @@
 import * as threeD from '../../public/modules/3D.js'
 
+// This can possible be changed, so it is made to fail.
+test('ourId uninitialised before init3D is called', () => {
+expect(threeD.ourID).toBe(undefined);
+})
+
 test('changeUserPosition - small value - all axis', () => {
+  let id = 3;
+  
+  threeD.init3D(id, {"5" : {id: 5}}, document.body);
+  expect(threeD.ourID).toBe(id);
 
-  threeD.init3D(3, {"5" : {id: 5}}, document.body);
-
-  threeD.newUserJoined3D(5, "test");
-
-  threeD.changeUserPosition(5, 1, 1, 1);
-
-  expect(threeD.UserMap[5].avatar.model.position.x).toBe(10);
+  threeD.newUserJoined3D(5, "myName");
+  expect(threeD.UserMap[5].name).toBe("myName");
+  expect(threeD.UserMap[5].avatar.model.scale.x).toBe(threeD.objectScale);
+  
+  threeD.changeUserPosition(5, 1, 2, 3);
+  expect(threeD.UserMap[5].avatar.model.position.x).toBe(1);
+  expect(threeD.UserMap[5].avatar.model.position.y).toBe(2);
 });
